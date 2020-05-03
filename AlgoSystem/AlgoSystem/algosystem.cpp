10eed0380fdcedfdb7c73e1a4fcbe913b810d813
Sapir Shemer <lesscomplexity@gmail.com>
2020-05-03 15:27:01 +0300

    First shit added

---
+++ AlgoSystem/AlgoSystem/algosystem.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Lotus on 03/05/2020.
+//
+
+#include <iostream>
+#define EXIT_COMMAND 2
+
+int main() {
+    int command = 0;
+
+    while(command != EXIT_COMMAND) {
+        std::cout << "";
+    }
+}
