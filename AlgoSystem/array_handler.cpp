3aa105085a12d0072aa430d49be629fbd015e30b
Perash14 <perash98@gmail.com>
2020-05-22 13:47:41 +0300

    niga

--- AlgoSystem/array_handler.cpp
+++ AlgoSystem/array_handler.cpp
@@ -7,32 +7,81 @@
 
 using namespace std;
 
-int* addElement(int* arr, int& size) {
+int RecieveElement(){
     int element;
     cout <<  "Which number would you like to add: ";
     cin >> element;
+    return element;
+}
+
+
+int* addElement(int* current_arr, int& size, int element) {
 
     int* newArr = new int[size + 1];
     for(int i = 0;i < size;i++){
-        newArr[i] = arr[i];
+        newArr[i] = current_arr[i];
     }
     newArr[size] = element;
-    delete arr;
+    delete current_arr;
     size++; // Changes the original
     return newArr; // Returns the new array
+}
+
+
+void printArray(int* current_arr, int size){
+    cout << "Your numbers are: ";
+    for(int i = 0;i < size - 1;i++) cout << current_arr[i] << ",";
+    cout << current_arr[size - 1] << "." << endl;
+}
+
+
+//Unfinished: Need to finish sortarray first and then
+void removeElement(int* current_arr, int& size , bool sorted){
+    //Find out if the array is sorted first, if yes then BinarySearch, if not! fuck zicaron and go over em all.
+    int ToFind;
+    cout << "This is your array :";
+    for(int i = 0;i < size - 1;i++) cout << current_arr[i] << ",";
+    cout << current_arr[size - 1] << "." << endl;
+
+    int* RecurringValueIndexArr = new int[0];
+    int RecurringValueIndexArrSize = 0;
+
+    while(RecurringValueIndexArrSize == 0){
+        cout << "Which number would you like to delete? ";
+        cin >> ToFind;
+        if(sorted == false){
+            //Making an array out of the indexes of the reouccuring value if it occurs more than once in current arr
+
+            int RecurringValueIndexArrSize = 0;
+            //Going over current arr to find the recurring indexes of tofind
+            for(int i = 0;i < size;i++){
+                if(current_arr[i] == ToFind){
+                    addElement(RecurringValueIndexArr, RecurringValueIndexArrSize, i);
+                }
+            }
+            if(RecurringValueIndexArrSize == 0) cout << "This value doesn't exist within Your array, dipshit, try again" << endl; continue;
+            else if(RecurringValueIndexArrSize == 1){}
+            else{}//If the value exists more than once in currentarr then ask hime which ones he would like to delete
+        }
+    }
+
+    else{
+        //Use the searcharray function
+    }
 }
 
 void printArrayHelp() {
     cout << "Which changes do you wish to make:" << endl <<
          "\t" << "|0 - Add|" << endl <<
          "\t" << "|1 - Remove|" << endl <<
-         "\t" << "|2 - Sort|" << endl <<
-         "\t" << "|3 - Search|" << endl <<
-         "\t" << "|4 - Back|" << endl;
+         "\t" << "|2 - Print|" << endl <<
+         "\t" << "|3 - Sort|" << endl <<
+         "\t" << "|4 - Search|" << endl <<
+         "\t" << "|5 - Back|" << endl;
 }
 
 void arrayCommand() {
-    int arr_size;
+    int arr_size = 0;
     bool is_sorted = false;
     int command = -1;
     int* arr = new int[0];
@@ -56,11 +105,11 @@ void arrayCommand() {
         }
 
         switch(command) {
-            case 0: arr = addElement(arr, arr_size); break;
-            case 1: removeElement(arr, arr_size); break;
+            case 0: arr = addElement(arr, arr_size, RecieveElement()); break;
+            case 1: removeElement(arr, arr_size, is_sorted); break;
             case 2: printArray(arr, arr_size); break;
-            case 3: sortArray(arr, arr_size); break;
-            case 4: searchArray(arr, arr_size, is_sorted); break;
+            /*case 3: sortArray(arr, arr_size); break;
+            case 4: searchArray(arr, arr_size, is_sorted); break;*/
         }
     }
 }
