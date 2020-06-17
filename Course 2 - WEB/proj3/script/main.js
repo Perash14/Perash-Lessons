386eca751ba9f1c45a07d823084276aefaf44e35
Sapir Shemer <lesscomplexity@gmail.com>
2020-06-17 16:14:18 +0300

    Alignments

---
+++ Course 2 - WEB/proj3/script/main.js
@@ -0,0 +1,20 @@
+let arr = [ "car", "gay" ];
+
+let obj = {
+    perash: "gay",
+    sapir: "not_gay",
+    lol: {
+        haha: "laugh",
+        no: 10
+    }
+}
+
+let post = {
+    id: 0,
+    content: "",
+    media: "",
+    likes: [ 10, 34, 23 ]
+}
+
+console.log(obj['lol']);
+console.log(post);
