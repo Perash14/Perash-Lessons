b4699c0babecb21346376c7e10bd43f9558a3a06
Sapir Shemer <lesscomplexity@gmail.com>
2020-06-22 16:14:43 +0300

    Lesson on JQuery

--- Course 2 - WEB/proj3/script/main.js
+++ Course 2 - WEB/proj3/script/main.js
@@ -18,3 +18,29 @@ let post = {
 
 console.log(obj['lol']);
 console.log(post);
+let init_height = 0;
+
+$(document).ready(function() {
+    // Drop down menu logic
+    $('.dropdown').hide();
+    init_height = $('#nav').height();
+    
+    $('#test').hover(
+        function() { // When hover
+            $('.dropdown').fadeIn(500);
+            $('#test').height($('#test').height() + $('.dropdown').height());
+            $('#nav').height(init_height);
+        },
+        function() { // When hover finishes
+            $('.dropdown').fadeOut(500);
+            $('#test').height($('#test').height() - $('.dropdown').height());
+        }
+    );
+
+    // Box game logic
+    let rand = Math.random() * ($(window).height() - $('#box').height());
+    $('#box').css({
+        'margin-top': rand + 'px'
+    });
+    console.log(rand);
+});
