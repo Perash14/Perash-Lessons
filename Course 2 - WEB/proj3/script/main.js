bc1de33c8dd17a5a495ab572e2144d70faf924aa
Sapir Shemer <lesscomplexity@gmail.com>
2020-06-24 16:13:56 +0300

    Animations JS lesson

--- Course 2 - WEB/proj3/script/main.js
+++ Course 2 - WEB/proj3/script/main.js
@@ -16,6 +16,10 @@ let post = {
     likes: [ 10, 34, 23 ]
 }
 
+let box_game = {
+
+};
+
 console.log(obj['lol']);
 console.log(post);
 let init_height = 0;
@@ -38,9 +42,39 @@ $(document).ready(function() {
     );
 
     // Box game logic
-    let rand = Math.random() * ($(window).height() - $('#box').height());
+    let rand = Math.floor($('#box').height() + Math.random() * ($(window).height() - 2 * $('#box').height()));
     $('#box').css({
-        'margin-top': rand + 'px'
+        'top': rand + 'px'
     });
-    console.log(rand);
 });
+
+
+function game_step(element) {
+    let pos_y = element.style.top;
+    let pos_x = element.style.left;
+    let pos_x_new = Math.floor(element.offsetWidth + Math.random() * ($(window).width() - 2 * element.offsetWidth));
+    let pos_y_new = Math.floor(element.offsetHeight + Math.random() * ($(window).height() - 2 * element.offsetHeight));
+    let id = setInterval(function() { animate(); }, 1);
+
+    function animate() {
+        // When the animation stops
+        if(pos_x == pos_x_new && pos_y == pos_y_new) {
+            clearInterval(id);
+        }
+        else {
+            if(pos_y != pos_y_new) {
+               if(pos_y > pos_y_new) pos_y--;
+               else pos_y++;
+            }
+            if(pos_x != pos_x_new) {
+                if(pos_x > pos_x_new) pos_x--;
+                else pos_x++;
+            }
+            // Update prev value
+            $('#box').css({
+                'top' : pos_y + 'px',
+                'margin-left' : pos_x + 'px'
+            });
+        }
+    }
+}
