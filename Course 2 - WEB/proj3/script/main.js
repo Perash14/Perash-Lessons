4119222c23c6a1734f74b05d66bf96e14a876d49
Sapir Shemer <lesscomplexity@gmail.com>
2020-06-29 15:08:52 +0300

    Lesson on animations and movement

--- Course 2 - WEB/proj3/script/main.js
+++ Course 2 - WEB/proj3/script/main.js
@@ -9,12 +9,27 @@ let obj = {
     }
 }
 
+// Object
 let post = {
     id: 0,
-    content: "",
-    media: "",
-    likes: [ 10, 34, 23 ]
+    content: "My post",
+    media: "my_image.png",
+    likes: [ 10, 34, 23 ],
+    comments: {
+        'user1': {
+            content: 'My Comment'
+        },
+        'user2': {
+            content: 'My Comment'
+        }
+    }
 }
+
+// Accessing a key in JS
+console.log(post['content']);
+console.log(post.content);
+console.log(post.likes);
+console.log(post.comments.user1.content);
 
 let box_game = {
 
@@ -24,12 +39,20 @@ console.log(obj['lol']);
 console.log(post);
 let init_height = 0;
 
+/**
+ * A function that runs only after the HTML document has been loaded fully
+ * Used to retrieve data about element after they are present in the document to avoid bugs
+ */
 $(document).ready(function() {
+    // $('element').some_function_or_property_of_that_element(); -> <element> </element>
+    // $('#some_id').some_function_or_property_of_that_element(); -> <element id="some_id"> </element>
+    // $('.some_class').some_function_or_property_of_that_element(); -> <element class="some_class"> </element>
+
     // Drop down menu logic
     $('.dropdown').hide();
-    init_height = $('#nav').height();
+    init_height = $('#nav').height(); // Getting a property
     
-    $('#test').hover(
+    $('#test').hover( // Running a function
         function() { // When hover
             $('.dropdown').fadeIn(500);
             $('#test').height($('#test').height() + $('.dropdown').height());
@@ -42,18 +65,30 @@ $(document).ready(function() {
     );
 
     // Box game logic
-    let rand = Math.floor($('#box').height() + Math.random() * ($(window).height() - 2 * $('#box').height()));
+    let rand_x = Math.floor(Math.random() * ($(window).height() - 2 * $('#box').height()));
+    let rand_y = Math.floor(Math.random() * ($(window).width() - 2 * $('#box').width()));
     $('#box').css({
-        'top': rand + 'px'
+        'margin-top': rand_x + 'px',
+        'margin-left': rand_y + 'px'
     });
 });
 
 
 function game_step(element) {
-    let pos_y = element.style.top;
-    let pos_x = element.style.left;
-    let pos_x_new = Math.floor(element.offsetWidth + Math.random() * ($(window).width() - 2 * element.offsetWidth));
-    let pos_y_new = Math.floor(element.offsetHeight + Math.random() * ($(window).height() - 2 * element.offsetHeight));
+    let pos_y = element.style.marginTop;
+    let pos_x = element.style.marginLeft;
+
+    // Change pos_x, pos_y to numbers
+    if(typeof pos_y == 'string' && pos_y.indexOf('px') != -1) {
+        pos_y = parseInt(pos_y.substring(0, pos_y.indexOf('px')));
+    }
+    if(typeof pos_x == 'string' && pos_x.indexOf('px') != -1) {
+        pos_x = parseInt(pos_x.substring(0, pos_x.indexOf('px')));
+    } else pos_x = 0;
+
+
+    let pos_x_new = Math.floor(Math.random() * ($(window).width() - 2 * element.offsetWidth));
+    let pos_y_new = Math.floor(Math.random() * ($(window).height() - 2 * element.offsetHeight));
     let id = setInterval(function() { animate(); }, 1);
 
     function animate() {
@@ -72,7 +107,7 @@ function game_step(element) {
             }
             // Update prev value
             $('#box').css({
-                'top' : pos_y + 'px',
+                'margin-top' : pos_y + 'px',
                 'margin-left' : pos_x + 'px'
             });
         }
